ddd549c6094800730a78249cb627ee1d87272c3b
Mattdev92 <mateuszgrzejszczyk1@gmail.com>
2021-06-15 17:48:58 +0200

    redux toolkit simple config finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { todoAdded } from 'slices/createSliceTodo';
 
 function App() {
-  const asas = useSelector((state) => state);
+  const dispatch = useDispatch();
+  const asas = useSelector(({ todos }) => todos);
   console.log(asas);
   return (
     <div className="App">
       <h1>Hello World</h1>
+      <button onClick={() => dispatch(todoAdded('Mat'))}>Click</button>
     </div>
   );
 }
