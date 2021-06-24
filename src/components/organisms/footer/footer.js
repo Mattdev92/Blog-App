62bb654adf61c4cb25b7180f5a15ddc17f7ea8cb
Mattdev92 <mateuszgrzejszczyk1@gmail.com>
2021-06-24 19:22:22 +0200

    add readme file

--- src/components/organisms/footer/footer.js
+++ src/components/organisms/footer/footer.js
@@ -1,9 +1,7 @@
 import { FooterWrapper } from 'components/organisms/footer/footer.styles';
 
 const Footer = () => {
-  return (
-    <FooterWrapper>&copy; Copyright 2021, Mateusz Grzejszczyk</FooterWrapper>
-  );
+  return <FooterWrapper>&copy; Copyright 2021, Blog for anybody</FooterWrapper>;
 };
 
 export default Footer;
